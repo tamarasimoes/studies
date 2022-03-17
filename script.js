cd768c2a44c4e8dcdc59388bdfc4f45f2a8ee633
Tamara Simões <tamara.simoes@betrybe.com>
2022-03-17 14:02:56 -0300

    site surpresa

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
 // Pegando o elemento
-let headerText = document.querySelector('#header');
+const headerText = document.querySelector('#header');
 
 // Criando o elemento que será adicionado
-let news = document.createElement('h1');
+const news = document.createElement('h1');
 
 // Adicionando a informação
 news.innerText = 'Olá, mundo!';
 
-//Inserindo no HTML
+// Inserindo no HTML
 headerText.appendChild(news);
 
-console.log(news);
-
-let list = ['Tamara', 'Belo Horizonte', '34', 'Buritis', 'Especialista', 'Cruzeiro'];
+const list = ['Tamara', 'Belo Horizonte', '34', 'Buritis', 'Especialista', 'Cruzeiro'];
 
 // Captura o pai
-let addArray = document.querySelector('#lista');
+const addArray = document.querySelector('#lista');
 
 // Criar o filho com cada item da array
-for (let index = 0; index < list.length; index++) {
-    let item = list[index];
-    let addArrayItem = document.createElement('li');
-    addArrayItem.innerText = item;
-    addArray.appendChild(addArrayItem);
-
+for (let index = 0; index < list.length; index += 1) {
+  const item = list[index];
+  const addArrayItem = document.createElement('li');
+  addArrayItem.innerText = item;
+  addArray.appendChild(addArrayItem);
 }
 
-function changeColor() {
-    news.style.backgroundColor = 'black';
-    news.style.color = 'white';
-    news.style.padding = '10px';
-
-}
+// function changeColor() {
+//   news.style.backgroundColor = 'black';
+//   news.style.color = 'white';
+//   news.style.padding = '10px';
+// }
